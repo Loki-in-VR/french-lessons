1ca7382f87c1eff1c45965bbdcb8eb996c6504b7
Loki-in-VR <pro.julien.meyer@gmail.com>
2023-05-26 13:08:59 +0200

    404 and password verification

---
+++ 404.js
@@ -0,0 +1,58 @@
+let user = undefined;
+let clickCounter = 0;
+let timeoutId = undefined;
+
+document.addEventListener('click', clickHandler);
+
+function clickHandler() {
+    if (typeof timeoutId === "number") {
+        clearTimeout(timeoutId);
+    }
+
+    timeoutId = setTimeout(() => {
+        clickCounter = 0;
+        timeoutId = undefined;
+    }, 500);
+
+    clickCounter++;
+
+    if (clickCounter === 7) {
+        document.removeEventListener('click', clickHandler)
+        clearTimeout(timeoutId);
+
+        document.body.style.backgroundColor = '#e0e0e0';
+        document.body.innerHTML = '<div id="password-field"><label for="password" id="password-label">Enter your password</label><input type="password" id="password"></div>';
+        document.getElementById('password').focus();
+
+        document.addEventListener('keyup', verifyPassword);
+    }
+}
+
+function verifyPassword() {
+    const password = document.getElementById('password').value;
+
+    if (password.length >= 12) {
+        const verifyPasswordRequest = new XMLHttpRequest();
+        verifyPasswordRequest.open('POST', 'verify-password', true);
+        verifyPasswordRequest.addEventListener('load', (event) => {
+            const verifyPasswordResponse = JSON.parse(event.target.response);
+    
+            if (typeof verifyPasswordResponse === 'string') {
+                user = verifyPasswordResponse;
+                document.removeEventListener('keyup', verifyPassword);
+
+                const revealWebsiteRequest = new XMLHttpRequest();
+                revealWebsiteRequest.open('PUT', 'index.html', true);
+                revealWebsiteRequest.addEventListener('load', (event) => {
+                    document.body.innerHTML = event.target.response;
+
+                    const script = document.createElement('script');
+                    script.setAttribute('src', 'script.js');
+                    document.body.append(script);
+                });
+                revealWebsiteRequest.send();
+            }
+        });
+        verifyPasswordRequest.send(password);
+    }
+}
