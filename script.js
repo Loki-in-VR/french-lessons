c98325df1da116e1984e47a63530912793da7efc
Loki-in-VR <pro.julien.meyer@gmail.com>
2023-06-02 17:05:33 +0200

    Static conversation page

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-document.querySelector('h1').textContent = `Hi ${user}!`;
+const title = document.querySelector('h1');
+const form = document.querySelector('form');
+const message = document.querySelector('textarea');
+
+title.textContent = `Hi ${user}!`;
+window.scrollTo(0, document.body.scrollHeight);
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    console.log(message.value);
+    message.value = '';
+});
